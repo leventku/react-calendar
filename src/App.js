2546936494b4377abd9cf7eba869223231526304
Levent Kucukgorkey <levent.kucukgorkey@gmail.com>
2016-11-25 22:38:35 +0000

    applied category as bg-color. renamed to event.json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import './App.css';
-import events from './events';
+import events from './events.json';
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
@@ -14,14 +14,18 @@ BigCalendar.setLocalizer(
 );
 
 class App extends Component {
+  eventGetter(eventObj) {
+    return { style: {backgroundColor: eventObj.category} }
+  }
   render() {
     return (
       <BigCalendar
         {...this.props}
         events={events}
+        eventPropGetter={this.eventGetter}
         titleAccessor='label'
         defaultDate={new Date(2016, 11, 1)}
-      />
+        />
     );
   }
 }
