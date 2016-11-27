ad7ffd81cd01fdaf58aac4abd458ab379f858ab6
Levent Kucukgorkey <levent.kucukgorkey@gmail.com>
2016-11-27 02:07:38 +0000

    added event json from remote host. Switched to full screen responsive styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import axios from 'axios';
+
 import './App.css';
 import events from './events.json';
 
@@ -14,14 +16,26 @@ BigCalendar.setLocalizer(
 );
 
 class App extends Component {
+
+  componentWillMount() {
+    axios.get('https://assessments.bzzhr.net/calendar/?format=json')
+      .then(response => {
+        if (response.data) {
+          this.setState({ events: response.data })
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        this.setState({ events: events })
+      });
+  }
   eventGetter(eventObj) {
-    return { style: {backgroundColor: eventObj.category} }
+    return { style: { backgroundColor: eventObj.category } }
   }
   render() {
     return (
       <BigCalendar
-        {...this.props}
-        events={events}
+        events={this.state ? this.state.events : []}
         eventPropGetter={this.eventGetter}
         titleAccessor='label'
         defaultDate={new Date(2016, 11, 1)}
